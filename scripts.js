c10aefb3b1f9a47d4b9646ab260ff3e4c61f625e
DanielusMagnificus <like3venom@gmail.com>
2022-06-13 17:24:55 +0200

    movies homework simple

--- scripts.js
+++
@@ -1,8 +0,0 @@
-let numberToGuess = Math.floor(Math.random() * 10)
-
-function guessNumber() {
-    let unos = parseInt(document.getElementById("unos").value);
-    console.log(unos);
-    unos == numberToGuess ? console.log("POGODAK") : unos ? unos > numberToGuess ? console.log("TRAZENI BROJ JE MANJI") : console.log("TRAZENI BROJ JE VECI") : console.log("NEISPRAVAN UNOS")
-}
-
